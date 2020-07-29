c34d21213cc8b49df628977fcf34d46b9fcee3bf
zriverbird <774986925@qq.com>
2020-07-29 22:28:58 +0800

    構建項目,創建element-ui

---
+++ src/views/login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+      <el-button>anniu</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#login {
+    height: 50vh;
+    background-color: #344a5f;
+}
+</style>
